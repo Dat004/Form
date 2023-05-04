66d8c6754d0d7f79192e3d4f2a94315f14e61630
Dat004 <119869560+Dat004@users.noreply.github.com>
2023-05-04 14:20:58 +0700

    Add files via upload

---
+++ Form login/main.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container')
+const btnSignIn = document.querySelector('.btnSign-in')
+const btnSignUp = document.querySelector('.btnSign-up')
+
+btnSignIn.addEventListener('click', () => {
+    container.classList.add('active')
+})
+
+btnSignUp.addEventListener('click', () => {
+    container.classList.remove('active')
+})
